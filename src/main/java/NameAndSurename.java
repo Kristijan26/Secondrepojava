854a002b4f10d6b7e5c7e0014fbf08b4b86b03cd
Kritijan <kbojlevski41@gmail.com>
2024-03-04 10:35:46 +0100

    Add NameAndSurename.java

---
+++ src/main/java/NameAndSurename.java
@@ -0,0 +1,10 @@
+public class NameAndSurename {
+    public static void main(String[] args) {
+        String ime = "Кристијан";
+        String prezime = "Бојлевски";
+        
+        System.out.println("Име: " + ime);
+        System.out.println("Презиме: " + prezime);
+    }
+}
+
